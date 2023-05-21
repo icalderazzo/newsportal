b43c64e5995588f48f6d41dcf36b9186fb7f0e8d
icalderazzo <ignaciocalderazzo@gmail.com>
2023-05-21 12:15:07 -0300

    Pagination Helper now adds search string to previous/next page building support for search string

--- backend/src/NewsPortal.Backend.WebApi/Helpers/PaginationHelper.cs
+++ backend/src/NewsPortal.Backend.WebApi/Helpers/PaginationHelper.cs
@@ -12,19 +12,20 @@ public static class PaginationHelper
     /// <param name="paginationFilter">The pagination filter used to page data.</param>
     /// <param name="totalRecords">Total record count.</param>
     /// <param name="baseUri">Base URI of the resource.</param>
+    /// <param name="searchString"></param>
     /// <typeparam name="T">Type of the object of the response.</typeparam>
     /// <returns></returns>
     public static PagedResponse<List<T>> CreatePagedResponse<T>(
-        List<T> data, PaginationFilter paginationFilter, int totalRecords, Uri baseUri)
+        List<T> data, PaginationFilter paginationFilter, int totalRecords, Uri baseUri, string? searchString = null)
     {
         var response = new PagedResponse<List<T>>(data, paginationFilter.PageNumber, paginationFilter.PageSize);
         var totalPages = Convert.ToInt32(totalRecords / paginationFilter.PageSize);
 
         response.NextPage = paginationFilter.PageNumber >= 1 && paginationFilter.PageNumber < totalPages 
-            ? BuildPageUri(paginationFilter.PageNumber + 1, paginationFilter.PageSize, baseUri)
+            ? BuildPageUri(paginationFilter.PageNumber + 1, paginationFilter.PageSize, baseUri, searchString)
             : null;
         response.PreviousPage = paginationFilter.PageNumber > 1 && paginationFilter.PageNumber <= totalPages 
-            ? BuildPageUri(paginationFilter.PageNumber - 1, paginationFilter.PageSize, baseUri)
+            ? BuildPageUri(paginationFilter.PageNumber - 1, paginationFilter.PageSize, baseUri, searchString)
             : null;
         
         response.TotalPages = totalPages;
@@ -33,11 +34,12 @@ public static class PaginationHelper
         return response;
     }
 
-    private static Uri BuildPageUri(int pageNumber, int pageSize, Uri baseUri)
+    private static Uri BuildPageUri(int pageNumber, int pageSize, Uri baseUri, string? searchString = null)
     {
+        var searchStringParam = string.IsNullOrEmpty(searchString) ? string.Empty : $"{searchString}&"; 
         var builder = new UriBuilder(baseUri)
         {
-            Query = $"pageNumber={pageNumber}&pageSize={pageSize}"
+            Query = $"{searchStringParam}pageNumber={pageNumber}&pageSize={pageSize}"
         };
         return builder.Uri;
     }
