66c3d782a9d92088976243b6396359335a86f7a7
icalderazzo <ignaciocalderazzo@gmail.com>
2023-05-22 22:01:41 -0300

    Update Items implementation in ItemsCacheService

--- backend/src/NewsPortal.Backend.Application/Item/ItemsCacheService.cs
+++ backend/src/NewsPortal.Backend.Application/Item/ItemsCacheService.cs
@@ -17,35 +17,61 @@ public class ItemsCacheService : IItemsCacheService
     public async Task<List<T>> GetOrCreateItems<T>(
         IEnumerable<int> itemIds, Func<int, Task<T?>> createItemFunc) where T : ItemDto
     {
-        //  Items concurrent collection
-        var items = new ConcurrentQueue<T>();
-
-        //  Execute get or create async in parallel 
-        await Parallel.ForEachAsync(itemIds ,async(itemId, cancellationToken) =>
+        try
         {
-            //  Get Item from cache or call the API
-            var item = await _memoryCache.GetOrCreateAsync<T>(
-                itemId,
-                async entry =>
-                {
-                    //  Get Item by its id
-                    var createItemResult = await createItemFunc(itemId);
-                    if (createItemResult is not null)
+            //  Items concurrent collection
+            var items = new ConcurrentQueue<T>();
+
+            //  Execute get or create async in parallel 
+            await Parallel.ForEachAsync(itemIds ,async(itemId, cancellationToken) =>
+            {
+                //  Get Item from cache or call the API
+                var item = await _memoryCache.GetOrCreateAsync<T>(
+                    itemId,
+                    async entry =>
                     {
-                        //  Add it to the cache for 10 min if the request succeeded
-                        entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10);
-                        return createItemResult;
-                    }
+                        //  Get Item by its id
+                        var createItemResult = await createItemFunc(itemId);
+                        if (createItemResult is not null)
+                        {
+                            //  Add it to the cache for 10 min if the request succeeded
+                            entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10);
+                            return createItemResult;
+                        }
                     
-                    //  Add an empty object with zero life time if request failed
-                    entry.AbsoluteExpirationRelativeToNow = TimeSpan.Zero;
-                    return default!;
-                });
+                        //  Add an empty object with zero life time if request failed
+                        entry.AbsoluteExpirationRelativeToNow = TimeSpan.Zero;
+                        return default!;
+                    });
             
-            //  Add item to the queue
-            if (item is not null && item.Id > 0) items.Enqueue(item);
-        });
+                //  Add item to the queue
+                if (item is not null && item.Id > 0) items.Enqueue(item);
+            });
         
-        return items.ToList();
+            return items.ToList();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    public async Task UpdateItems<T>(
+        IEnumerable<int> itemIds, Func<int, Task<T?>> getUpdatedItemFunc) where T : ItemDto
+    {
+        await Parallel.ForEachAsync(itemIds,async (itemId, cancellationToken) =>
+        {
+            var item = _memoryCache.Get<T>(itemId);
+            if (item is not null)
+            {
+               var updatedItem = await getUpdatedItemFunc(itemId);
+               if (updatedItem is not null)
+               {
+                   _memoryCache.Remove(itemId);
+                   _memoryCache.Set(itemId, updatedItem);
+               }
+            }
+        });
     }
 }
--- backend/src/NewsPortal.Backend.Application/Services/IItemsCacheService.cs
+++ backend/src/NewsPortal.Backend.Application/Services/IItemsCacheService.cs
@@ -11,4 +11,13 @@ public interface IItemsCacheService
     /// <param name="createItemFunc">Delegate to create items in case they're not present in the cache.</param>
     /// <returns></returns>
     Task<List<T>> GetOrCreateItems<T>(IEnumerable<int> itemIds, Func<int, Task<T?>> createItemFunc) where T : ItemDto;
+
+    /// <summary>
+    ///     Updates the specified items in the cache.
+    /// </summary>
+    /// <param name="itemIds">The specified items id list.</param>
+    /// <param name="getUpdatedItemFunc">Delegate to get an updated item.</param>
+    /// <typeparam name="T">Type of the item to update.</typeparam>
+    /// <returns></returns>
+    Task UpdateItems<T>(IEnumerable<int> itemIds, Func<int, Task<T?>> getUpdatedItemFunc) where T : ItemDto;
 }
