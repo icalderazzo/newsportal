87120de523848a5f16754068527a23d09f2a82a8
Ignacio Calderazzo <ignaciocalderazzo@gmail.com>
2026-02-10 16:05:44 -0300

    ng 19

--- frontend/src/app/stories/storycard/storycard.component.ts
+++ frontend/src/app/stories/storycard/storycard.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'story-card',
-  templateUrl: './storycard.component.html'
+    selector: 'story-card',
+    templateUrl: './storycard.component.html',
+    standalone: false
 })
 export class StorycardComponent {
   @Input() title: string | undefined;
