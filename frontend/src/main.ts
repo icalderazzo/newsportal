f2a7f0f5fe05077d90fa97bc0503a55f30b2f744
Ignacio Calderazzo <ignaciocalderazzo@gmail.com>
2026-02-10 16:12:42 -0300

    Final angular 21 update

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,8 @@
 import { provideZoneChangeDetection } from "@angular/core";
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { platformBrowser } from "@angular/platform-browser";
 
 
-platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
+platformBrowser().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => console.error(err));
